fb48c2a76f2cae15b064dd90148934bf18e1fcce
CortezSMz <cortez_alexandre@hotmail.com>
2022-04-05 22:03:56 -0300

    move github link to footer

--- src/components/Controls.vue
+++ src/components/Controls.vue
@@ -1,58 +1,66 @@
 <template>
   <v-footer color="primary lighten-1" app>
-    <v-row justify="center" align="center" class="text-center">
-      <v-col cols="12">
+    <v-row justify="center" align="center" class="footer-row text-center">
+      <v-col cols="2">
+        <v-btn
+          light
+          color="primary lighten-1"
+          fab
+          small
+          href="https://github.com/CortezSMz/connectfour"
+          target="blank"
+        >
+          <font-awesome-icon
+            icon="fa-brands fa-github"
+            transform="grow-2"
+            size="3x"
+            style="color: white"
+          />
+        </v-btn>
+      </v-col>
+
+      <v-col cols="8">
         <strong>Connect Four</strong>
       </v-col>
-      <v-spacer></v-spacer>
-    </v-row>
-
-    <div class="text-center">
-      <v-bottom-sheet v-model="controls">
-        <template v-slot:activator="{ on, attrs }">
-          <v-btn light fab small v-bind="attrs" v-on="on">
-            <font-awesome-icon
-              icon="gear"
-              size="2x"
-              :style="{ color: 'var(--v-primary-lighten1)' }"
-            />
-          </v-btn>
-        </template>
-        <v-sheet class="footer-sheet" color="primary lighten-1">
-          <v-row align="center" justify="center">
-            <v-col cols="12">
-              <h3>Connect Four</h3>
-              <p>
-                The objective of the game is to be the first to form a
-                horizontal, vertical, or diagonal line of four discs.
-              </p>
-            </v-col>
-            <v-col cols="12">
-              <v-btn
-                class="footer-btn"
-                rounded
-                light
-                href="https://github.com/CortezSMz/connectfour"
-                target="blank"
-                >source code</v-btn
-              >
-              <v-btn class="footer-btn" rounded light @click="resetGame"
-                >Reset game</v-btn
-              >
-            </v-col>
-          </v-row>
-          <v-row align="center" justify="center">
-            <v-btn class="footer-btn" small light fab v-if="false">
+      <v-col cols="2">
+        <v-bottom-sheet v-model="controls">
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn light fab small v-bind="attrs" v-on="on">
               <font-awesome-icon
-                icon="save"
+                icon="gear"
                 size="2x"
                 :style="{ color: 'var(--v-primary-lighten1)' }"
               />
             </v-btn>
-          </v-row>
-        </v-sheet>
-      </v-bottom-sheet>
-    </div>
+          </template>
+          <v-sheet class="footer-sheet" color="primary lighten-1">
+            <v-row align="center" justify="center">
+              <v-col cols="12">
+                <h3>Connect Four</h3>
+                <p>
+                  The objective of the game is to be the first to form a
+                  horizontal, vertical, or diagonal line of four discs.
+                </p>
+              </v-col>
+              <v-col cols="12">
+                <v-btn class="footer-btn" rounded light @click="resetGame"
+                  >Reset game</v-btn
+                >
+              </v-col>
+            </v-row>
+            <v-row align="center" justify="center">
+              <v-btn class="footer-btn" small light fab v-if="false">
+                <font-awesome-icon
+                  icon="save"
+                  size="2x"
+                  :style="{ color: 'var(--v-primary-lighten1)' }"
+                />
+              </v-btn>
+            </v-row>
+          </v-sheet>
+        </v-bottom-sheet>
+      </v-col>
+    </v-row>
   </v-footer>
 </template>
 
--- src/plugins/fontawesome.ts
+++ src/plugins/fontawesome.ts
@@ -1,8 +1,9 @@
 import Vue from "vue";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faGear, faSave, faXmark } from "@fortawesome/free-solid-svg-icons";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
-library.add(faGear, faSave, faXmark);
+library.add(faGear, faSave, faXmark, faGithub);
 
 Vue.component("font-awesome-icon", FontAwesomeIcon);
